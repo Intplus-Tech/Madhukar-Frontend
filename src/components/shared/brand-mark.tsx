f175c4a9171fa567cb67f62d8d6bd9e062b4a447
cyberrspace <xhamooel95@gmail.com>
2026-08-14 02:24:21 +0100

    fixing logo

--- src/components/shared/brand-mark.tsx
+++ src/components/shared/brand-mark.tsx
@@ -4,12 +4,12 @@ import { cn } from "@/lib/utils";
 export function BrandMark({ className }: { className?: string }) {
   return (
     <Image
-      src="/new-madhukar.png"
-      alt="new-madhukar"
-      width={112}
-      height={112}
-      priority
-      className={cn("h-28 w-28", className)}
-    />
+  src="/new-madhukar.png"
+  alt="Lakshya 72"
+  width={320}
+  height={320}
+  priority
+  className={cn("h-16 w-16 object-contain", className)}
+/>
   );
 }
